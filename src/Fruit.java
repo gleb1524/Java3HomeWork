19c282637a131283814120b8bb665f5bbee80210
gleb1524 <gleb1524@yandex.ru>
2022-01-30 13:40:26 +0300

    lesson1J3

---
+++ src/Fruit.java
@@ -0,0 +1,10 @@
+abstract class Fruit {
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+    private float weight = 0.0f;
+
+    public float getWeightFr() {
+        return weight;
+    }
+}
